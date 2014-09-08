ed853cdfd5943c3f4d76d8603b93eff2e9461fb3
cmongle <carrie.mongle@stonybrook.edu>
2014-09-08 15:37:17 -0400

    After changing percent difference function to match Sladeks, subscripts are now out of bounds.. trying to fix

--- midshaft_functions_currentAOVedits.R
+++ midshaft_functions_currentAOVedits.R
@@ -6,31 +6,24 @@ library(ggthemes)
 
 
 #Load and attach data ####
-#Load source file with all femur data
+#Load source file with all humerus data
 setwd("//Users/Carrie/Desktop/")
-femurdataframe <- read.table("Ape.Midshaft_Slice.Geometry.Ouput_Femur.txt", header=T, sep="\t")
-
-#Now we want only the "Human" data
-#use grep function to pull out labels with Human in beginning of character string
-Human.rows <- grep("^SBUhuman_", as.matrix(femurdataframe$label))
-
-#Additional code for humans to include Point Hope (currently not working for femur)
-#PH.rows <- grep("^PointHope_", as.matrix(femurdataframe$label))
-#combined <- c(Human.rows, PH.rows)
-
-#now pull out only those row levels which included Human
-Human.femurdataframe <- femurdataframe[Human.rows,]
+humerusdataframe <- read.table("Ape.Midshaft_Slice.Geometry.Ouput_humerus.txt", header=T, sep="\t")
 
 
-#Create rows for J and Imin/Imax
-Human.femurdataframe$J <- Human.femurdataframe$Imin + Human.femurdataframe$Imax
-Human.femurdataframe$ratio <- Human.femurdataframe$Imax / Human.femurdataframe$Imin
+#Now we want only the chimpanzee data
+#use grep function to pull out labels with Pan in beginning of character string
+pan.rows <- grep("^Pan_", as.matrix(humerusdataframe$label))
+#now pull out only those row levels which included pan
+pan.humerusdataframe <- humerusdataframe[pan.rows,]
+
+pan.humerusdataframe$J <- pan.humerusdataframe$Imin + pan.humerusdataframe$Imax
+pan.humerusdataframe$ratio <- pan.humerusdataframe$Imax / pan.humerusdataframe$Imin
 
 
 #dataframe must be attached for this function to work
-data <- Human.femurdataframe
+data <- (pan.humerusdataframe)
 attach(data)
-
 
 
 ##########################################################################################
@@ -271,7 +264,7 @@ midshaft.ratio.function <- function(x)
   ratio.levels <- data.frame(rbind(slice.matrix[,2]))
   ratio.levels <- cbind(x, ratio.levels)
   colnames(ratio.levels) <- c("specimen", seq(0,95, by=5))
-  #perdiff <- rbind(((csa.levels[,2:21] - csa.levels[,12])/csa.levels[,12])*100)/length(unique(x))
+  perdiff <- rbind(((ratio.levels[,2:21] - ratio.levels[,12])/ratio.levels[,12])*100)/length(unique(x))
   perdiff <- data.frame(cbind(x, perdiff))
   colnames(perdiff) <- c("specimen",seq(0,95, by=5))
   return(perdiff)
@@ -360,34 +353,3 @@ write.table(ratio.ttest, file="Human_Femur_ratio_ttest.txt", sep="\t")
 
 
 
-####################################################################################################################################################################################
-#****Need to edit to reflect Sladek formula: Combined Plot####
-pdf("Human Femur Combined.pdf", height=21, width=35)
-#Plot the mean differences (absolute) from the Tukey of the percent difference data, along with the standard deviations for those values
-result.diaphysis.plot <- ggplot(csa.midshaft.aov, aes(csa.midshaft.aov$level, csa.midshaft.aov$diff*100, group=1)) +
-  scale_colour_manual("", breaks=c("CSA","J","Imax/Imin"), values = c("blue", "orange", "dark red"))+
-  scale_fill_manual("", breaks=c("CSA","J","Imax/Imin"), values = c("light blue", "yellow", "red"))+
-  #CSA Line with Std.E bands
-  geom_line(stat='smooth', sHuman=.5, aes(colour="CSA", fill="CSA"), size=2) + 
-  geom_ribbon(aes(ymax=(csa.midshaft.aov$diff+ (csa.midshaft.aov$Std.E))*100 , ymin= (csa.midshaft.aov$diff - (csa.midshaft.aov$Std.E))*100, fill="CSA"), alpha=.2)+
-  #J Line with Std.E bands
-  geom_line(data= J.midshaft.aov,aes(level, diff*100, colour="J", fill="J"), stat='smooth', sHuman=.5, size=2) + 
-  geom_ribbon(data= J.midshaft.aov, aes(ymax=(J.midshaft.aov$diff+ (J.midshaft.aov$Std.E))*100 , ymin= (J.midshaft.aov$diff - (J.midshaft.aov$Std.E))*100, fill="J"), alpha=.2)+
-  #Imax/Imin Ratio Line with Std.E bands
-  geom_line(data= ratio.midshaft.aov,aes(level, diff*100, colour="Imax/Imin", fill="Imax/Imin"), stat='smooth', sHuman=.5, size=2) + 
-  geom_ribbon(data= ratio.midshaft.aov, aes(ymax=(ratio.midshaft.aov$diff+ (ratio.midshaft.aov$Std.E))*100 , ymin= (ratio.midshaft.aov$diff - (ratio.midshaft.aov$Std.E))*100, fill="Imax/Imin"), alpha=.3)
-
-
-#Add Theme
-result.diaphysis.plot + xlab('5% Intervals Along Diaphysis')+ ylab('Percent Difference from Midshaft') + theme_calc(base_size=28) + 
-  theme(axis.title.x = element_text(vjust = -.5), axis.title.y = element_text(vjust = .5, angle = 90)) 
-dev.off()
-
-
-
-##########################
-#More simple plot that Ian preferred ####
-ggplot(result.csa.long, aes(result.csa.long$level, result.csa.long$percent.difference, group=1)) +
-  geom_point() 
-
-#probably need to leave trunkated plot called result.csa.long on all files
