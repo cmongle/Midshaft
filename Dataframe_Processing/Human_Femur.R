5a13adec28a86fe6737065f6d397e4f488661565
cmongle <carrie.mongle@stonybrook.edu>
2014-09-10 14:35:30 -0400

    No longer having subscript problems with functions

--- Dataframe_Processing/Human_Femur.R
+++ Dataframe_Processing/Human_Femur.R
@@ -8,18 +8,18 @@ library(ggthemes)
 #Load and attach data ####
 #Load source file with all femur data
 setwd("//Users/Carrie/Desktop/")
-femurdataframe <- read.table("Ape.Midshaft_Slice.Geometry.Ouput_Femur.txt", header=T, sep="\t")
+femurdataframe <- read.table("Ape.Midshaft_Slice.Geometry.Ouput_femur.txt", header=T, sep="\t")
 
 #Now we want only the "Human" data
 #use grep function to pull out labels with Human in beginning of character string
 Human.rows <- grep("^SBUhuman_", as.matrix(femurdataframe$label))
 
 #Additional code for humans to include Point Hope (currently not working for femur)
-#PH.rows <- grep("^PointHope_", as.matrix(femurdataframe$label))
-#combined <- c(Human.rows, PH.rows)
+PH.rows <- grep("^PointHope_", as.matrix(femurdataframe$label))
+combined <- c(Human.rows, PH.rows)
 
 #now pull out only those row levels which included Human
-Human.femurdataframe <- femurdataframe[Human.rows,]
+Human.femurdataframe <- femurdataframe[combined,]
 
 
 #Create rows for J and Imin/Imax
