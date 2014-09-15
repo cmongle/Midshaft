c17036108858341cd7826e815312bda20b9f10f5
cmongle <carrie.mongle@stonybrook.edu>
2014-09-15 14:40:21 -0400

    ...

--- Midshaft.Analysis_currentPlotedits.R
+++ Midshaft.Analysis_currentPlotedits.R
@@ -144,3 +144,75 @@ result.plot + xlab('5% Intervals Along Diaphysis')+ ylab('Percent Difference fro
   theme(axis.title.x = element_text(vjust = -.4), axis.title.y = element_text(vjust = .7, angle = 90)) 
 
 dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+result.csa.long <- rbind(result.csa.long[which(result.csa.long$level == 30),], 
+                         result.csa.long[which(result.csa.long$level == 35),],
+                         result.csa.long[which(result.csa.long$level == 40),],
+                         result.csa.long[which(result.csa.long$level == 45),],
+                         result.csa.long[which(result.csa.long$level == 50),],
+                         result.csa.long[which(result.csa.long$level == 55),],
+                         result.csa.long[which(result.csa.long$level == 60),],
+                         result.csa.long[which(result.csa.long$level == 65),],
+                         result.csa.long[which(result.csa.long$level == 70),])
+
+
+
+m <- aov(terms(result.csa.long$percent.difference ~ result.csa.long$level + (result.csa.long$specimen)))
+csa.m.tuk <- TukeyHSD(m)
+
+#Pull out the mean percent differences from the TukeyHSD at each level of interest
+midshaft.aov <- c('50-30','50-35','50-40','50-45', '55-50', '60-50', '65-50','70-50')
+csa.midshaft.aov <- as.data.frame(csa.m.tuk$'result.csa.long$level'[midshaft.aov,])
+csa.midshaft.aov['50-50',] <- 0
+csa.midshaft.aov$level <- c(30, 35, 40, 45, 55, 60, 65, 70, 50)
+csa.midshaft.aov <- abs(csa.midshaft.aov)
+csa.midshaft.aov$SD <- (abs(csa.midshaft.aov$upr - csa.midshaft.aov$lwr))/2 #This is 2 standard deviations for a 95% confidence interval-- if doing 1 sd away from the mean, just divide this by 1/2
+csa.midshaft.aov$Std.E <- csa.midshaft.aov$SD/(sqrt(length(csa.midshaft.aov$SD)))
+
+
+
+
+#****Need to edit to reflect Sladek formula: Combined Plot####
+pdf("Human Femur Combined.pdf", height=21, width=35)
+#Plot the mean differences (absolute) from the Tukey of the percent difference data, along with the standard deviations for those values
+result.diaphysis.plot <- ggplot(csa.midshaft.aov, aes(csa.midshaft.aov$level, csa.midshaft.aov$diff, group=1)) +
+  scale_colour_manual("", breaks=c("CSA","J","Imax/Imin"), values = c("blue", "orange", "dark red"))+
+  scale_fill_manual("", breaks=c("CSA","J","Imax/Imin"), values = c("light blue", "yellow", "red"))+
+  #CSA Line with Std.E bands
+  geom_line(stat='smooth', sHuman=.5, aes(colour="CSA", fill="CSA"), size=2) + 
+  geom_ribbon(aes(ymax=(csa.midshaft.aov$diff+ (csa.midshaft.aov$Std.E)) , ymin= (csa.midshaft.aov$diff - (csa.midshaft.aov$Std.E)), fill="CSA"), alpha=.2)+
+  #J Line with Std.E bands
+  geom_line(data= J.midshaft.aov,aes(level, diff*100, colour="J", fill="J"), stat='smooth', sHuman=.5, size=2) + 
+  geom_ribbon(data= J.midshaft.aov, aes(ymax=(J.midshaft.aov$diff+ (J.midshaft.aov$Std.E))*100 , ymin= (J.midshaft.aov$diff - (J.midshaft.aov$Std.E))*100, fill="J"), alpha=.2)+
+  #Imax/Imin Ratio Line with Std.E bands
+  geom_line(data= ratio.midshaft.aov,aes(level, diff*100, colour="Imax/Imin", fill="Imax/Imin"), stat='smooth', sHuman=.5, size=2) + 
+  geom_ribbon(data= ratio.midshaft.aov, aes(ymax=(ratio.midshaft.aov$diff+ (ratio.midshaft.aov$Std.E))*100 , ymin= (ratio.midshaft.aov$diff - (ratio.midshaft.aov$Std.E))*100, fill="Imax/Imin"), alpha=.3)
+
+
+#Add Theme
+result.diaphysis.plot + xlab('5% Intervals Along Diaphysis')+ ylab('Percent Difference from Midshaft') + theme_calc(base_size=28) + 
+  theme(axis.title.x = element_text(vjust = -.5), axis.title.y = element_text(vjust = .5, angle = 90)) 
+dev.off()
+
+
+
+ggplot(csa.midshaft.aov, aes(csa.midshaft.aov$level, csa.midshaft.aov$diff, group=1)) +
+  
+  #CSA Line with Std.E bands
+  geom_line(stat='smooth', sHuman=.3,  size=2) + 
+  stat_smooth()
+geom_ribbon(aes(ymax=(csa.midshaft.aov$diff+ (csa.midshaft.aov$Std.E)) , ymin= (csa.midshaft.aov$diff - (csa.midshaft.aov$Std.E))),stat='smooth' ,alpha=.2)+
+  ylim(0,5)
+
+
